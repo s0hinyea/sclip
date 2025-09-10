f2d07d0c28c89423b1c4e4e2716e554a42b24d67
Mohammed Islam <mtislam5757@gmail.com>
2025-09-10 14:06:23 -0400

    add file uploading

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, File, UploadFile 
 #import FastAPI: class for creating app instance that we will add routes to
 import os 
+import librosa
+from moviepy.editor import VideoFileClip
+from audio import extract_audio
+from pathlib import Path
 
 app = FastAPI() 
 
@@ -11,31 +15,35 @@ async def root():
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
 
-  cwd = os.getcwd()
-  see_data = os.path.isdir("data")
 
   """posting to this route means uploading a file. 
   UploadFile => FastAPI type for file uploads
   File(...) => input should come from a file in the request
   Together: Expect the user to attach one file, call it file!
-  A video file is saved as a long sequence of bytes 
-
-  file_path = f"data/{file.filename}"
-  print(file_path)
+  A video file is saved as a long sequence of bytes """
+  
+  
+  BASE_DIR = Path(__file__).resolve().parent.parent
+  video_path = BASE_DIR / "data" / file.filename
+  print(video_path)
+  audio_path = BASE_DIR / "data" / "sample.wav"
   #the filepath: we will save it to the data folder with its original filename
+  
   content = await file.read() 
   #reads the file bytes from the upload object into memory
   #await lets us compelte other tasks while reading is done 
-
-  with open(file_path, "wb") as f:
+  with open(video_path, "wb") as f:
     f.write(content)
-     
-     '
+
+
+  """
       open(file_path, "wb") → “Create a new file at data/filename in write-binary mode.”
       If clip.mp4 already exists there, this will overwrite it.
       If it doesn’t exist, Python will make a new one.
       
-      '
-  
-  return {"filename": file.filename, "saved_to": file_path}"""
+    """
+  extract_audio(video_path, audio_path)
+  return {"video_name": file.filename, 
+          "saved_to": video_path,
+          "audio_array": audio_file}
 
